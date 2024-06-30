01e9ab0eeb6a2f30a2d5b3980dab48ceb32e2c0b
HANZALA SALEEM <hanzalabutt903@gmail.com>
2024-06-30 11:54:53 +0500

    29/06/2024

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./routes/user.route.js";
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO)
+	.then(() => {
+		console.log("Mongo DB connected");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+const app = express();
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
+
+// app.get("/test", (req, res) => {
+// 	res.send("Hello World");
+// });
+
+app.use("/user", router);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import Header from "./component/Header";
 
 export default function App() {
 	return (
-		<div>
-			<h1 className="text-3xl text-red-100">Hello World</h1>
-		</div>
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
